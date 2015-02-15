b6060927d1c3d2a70af5a17417261d658231d8fe
kamneo <alexandre.pinero@gmail.com>
2015-02-15 12:53:57 -0500

    Modification de l'aborescence, creation de fichiers anexes pour l'affichage et les methodes utiles

--- src/grid/grid.c
+++ src/grid/grid.c
@@ -12,6 +12,29 @@ struct grid_s{
 };
 
 /*
+ * \brief Calculate an random int beetween 0 and n
+ * \param n Indice max
+ * \return a random int beetween 0 and n
+ */
+static int rang_aleatoire (int n){
+	srand(time(NULL));
+	return rand()%n;
+}
+
+/*
+ * \brief Calculate a value, this value have 9/10 chance to be 1 and 1/10 to be 2
+ * \return an int between 1 and 2
+ */
+static int valeur_aleatoire (){
+	srand(time(NULL));
+
+	if (rand()%10 < 9)
+		return 1;
+	else
+		return 2;
+}
+
+/*
  * \brief Initialize grid structure
  * \return created an empty grid with score equal to 0
  */
@@ -27,6 +50,7 @@ grid new_grid ()
 	for(int i = 0; i < GRID_SIDE; i++)
         for(int j = 0; j < GRID_SIDE; j++)
             g->tiles[i][j] = 0;
+
 	return g;
 }
 
@@ -81,7 +105,7 @@ void set_score(grid g, unsigned long int x)
  * \param g the grid
  * \param x and y tile's coordinates
  * \return the tile
- * \pre 0 <= x < GRID_SIDE and 0 <= y < GRID_SIDE
+ * \pre 0 < = x < GRID_SIDE and 0 < = y < GRID_SIDE
  */
 
 tile get_tile (grid g, int x, int y){
@@ -108,7 +132,7 @@ void set_tile (grid g, int x, int y, tile t)
  */
 bool can_move (grid g, dir d)
 {
-	if( d == LEFT || d == RIGHT)
+	if( d  ==  LEFT || d  ==  RIGHT)
 	{
 		for (int i = 0; i < GRID_SIDE; i++)
 		{
@@ -117,7 +141,7 @@ bool can_move (grid g, dir d)
 		}
 	}
 
-	if( d == UP || d == DOWN)
+	if( d  ==  UP || d  ==  DOWN)
 	{
 		for (int j = 0; j < GRID_SIDE; j++)
 		{
@@ -143,7 +167,7 @@ bool game_over (grid g)
  * \brief Move every tiles of the grid in the direction specified by the user
  * \param g the grid
  * \param d the chosen direction
- * \pre the movement d must be possible (i.e. can_move(g,d) == true).
+ * \pre the movement d must be possible (i.e. can_move(g,d)  ==  true).
  */
 void do_move (grid g, dir d)
 {
@@ -153,7 +177,7 @@ void do_move (grid g, dir d)
 		return;
 	}
 
-	if( d == LEFT || d == RIGHT)
+	if( d  ==  LEFT || d  ==  RIGHT)
 	{
 		for (int i = 0; i < GRID_SIDE; i++)
 		{
@@ -161,7 +185,7 @@ void do_move (grid g, dir d)
 		}
 	}
 
-	if( d == UP || d == DOWN)
+	if( d  ==  UP || d  ==  DOWN)
 	{
 		for (int j = 0; j < GRID_SIDE; j++)
 		{
@@ -178,53 +202,34 @@ void do_move (grid g, dir d)
  * \pre grid g must contain  at least one empty tile.
  */
 
- static int rang_aleatoire (int n){
-	int nombreAleatoire;
-	srand(time(NULL));
-	nombreAleatoire=rand()%n;
-	return nombreAleatoire;
-}
-
-static int valeur_aleatoire (){
-	int nombreAleatoire;
-	srand(time(NULL));
-	nombreAleatoire=rand()%10;
-	if (nombreAleatoire<9)
-		nombreAleatoire=1;
-	else
-		nombreAleatoire=2;
-	return nombreAleatoire;
-}
-
-
 void add_tile (grid g){
 	int taille = (GRID_SIDE*GRID_SIDE)*2;
-	int nbFree=0;
+	int nbFree = 0;
 	int* tab;
 	int nombreAleatoire;
 	tab = malloc(sizeof(int)*taille);
 	for (int i = 0; i < GRID_SIDE; i++){
 		for (int j = 0; j < GRID_SIDE; j++)
 		{
-			if (g->tiles[i][j]==0)
+			if (g->tiles[i][j]  ==  0)
 			{
-				tab[nbFree*2]=i;
-				tab[nbFree*2+1]=j;
-				nbFree=nbFree+1;
+				tab[nbFree*2] = i;
+				tab[nbFree*2+1] = j;
+				nbFree += 1;
 			}		
 		}
 	}
-	if (nbFree==0)
+	if (nbFree  ==  0)
 		return;
-	nombreAleatoire=rang_aleatoire(nbFree)*2;
-	g->tiles[tab[nombreAleatoire]][tab[nombreAleatoire+1]]=valeur_aleatoire();
+	nombreAleatoire = rang_aleatoire(nbFree)*2;
+	g->tiles[tab[nombreAleatoire]][tab[nombreAleatoire+1]] = valeur_aleatoire();
 	free(tab);
 }
+
 /**
  * \brief Play a direction in the grid.
  * \param g the grid
  * \param d the direction
- * \pre the movement d must be possible (i.e. can_move(g,d) == true).
+ * \pre the movement d must be possible (i.e. can_move(g,d)  ==  true).
  */
-
 void play (grid g, dir d);
