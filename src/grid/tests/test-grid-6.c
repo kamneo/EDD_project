7baafd00dd5ca0191e32eebd42fcb6e3c0524f5e
Raitei3 <antoine24.borde@gmail.com>
2015-03-18 12:52:20 +0100

    test score

--- src/grid/tests/test-grid-6.c
+++ src/grid/tests/test-grid-6.c
@@ -23,10 +23,10 @@ int main (int argc, char** argv)
 	bool test_LEFT_ok = true;
 	bool test_RIGHT_ok = true;
 
-	set_tile (g, 0, 0, 2);
-	set_tile (g, 0, 1, 2);
-	set_tile (g, 0, 2, 4);
-	set_tile (g, 0, 3, 4);
+	set_tile (g, 0, 0, 3);
+	set_tile (g, 0, 1, 3);
+	set_tile (g, 0, 2, 2);
+	set_tile (g, 0, 3, 2);
 
 	set_tile (g, 1, 1, 2);
 	set_tile (g, 1, 2, 2);
@@ -35,42 +35,42 @@ int main (int argc, char** argv)
 	set_tile (g, 2, 3, 2);
 
 	set_tile (g, 3, 1, 2);
-	set_tile (g, 3, 3, 4);
+	set_tile (g, 3, 3, 3);
 
 
 	/*
 	 *      la grille vaut : 
 	 *		+--+--+--+--+
-	 *		| 2| 2| 4| 4| 
+	 *		| 4| 4| 16|16| 
 	 *		+--+--+--+--+
-	 *		| 0| 2| 2| 0| 
+	 *		| 0| 4| 4| 0| 
 	 *      +--+--+--+--+ 
 	 *		| 2| 0| 2| 0| 
 	 *		+--+--+--+--+
 	 *		| 0| 2| 0| 4| 
 	 *      +--+--+--+--+ 
 	 */
+	
 
 	do_move(g, LEFT);
 	if(get_tile(g, 0, 0) != 4)
 		test_LEFT_ok = false;
-	if(get_tile(g, 0, 1) != 8)
+	if(get_tile(g, 0, 1) != 3)
 		test_LEFT_ok = false;
-	if(get_tile(g, 1, 0) != 4)
+	if(get_tile(g, 1, 0) != 3)
 		test_LEFT_ok = false;
-	if(get_tile(g, 2, 0) != 4)
+	if(get_tile(g, 2, 0) != 3)
 		test_LEFT_ok = false;
 	if(get_tile(g, 3, 0) != 2)
 		test_LEFT_ok = false;
-	if(get_tile(g, 3, 1) != 4)
+	if(get_tile(g, 3, 1) != 3)
 		test_LEFT_ok = false;
 
 	// le score doit etre 20
-	if(grid_score(g) != 20)
+	if(grid_score(g) != 40)
 		test_LEFT_ok = false;
-
 	if(!test_LEFT_ok)
-		printf("erreur lors du do_move(LEFT) ou du grid_score(LEFT) ici le score doit valoir 20.(%d)\n", grid_score(g));
+		printf("erreur lors du do_move(LEFT) ou du grid_score(LEFT) ici le score doit valoir 40.(%lu)\n", grid_score(g));
 	
 	/*
 	 *      la grille vaut : 
@@ -85,23 +85,23 @@ int main (int argc, char** argv)
 	 *      +--+--+--+--+ 
 	 */
 	do_move(g, UP);
-	if(get_tile(g, 0, 0) != 8)
+	if(get_tile(g, 0, 0) != 4)
 		test_UP_ok = false;
-	if(get_tile(g, 0, 1) != 8)
+	if(get_tile(g, 0, 1) != 4)
 		test_UP_ok = false;
 	if(get_tile(g, 1, 0) != 4)
 		test_UP_ok = false;
-	if(get_tile(g, 1, 1) != 4)
+	if(get_tile(g, 1, 1) != 0)
 		test_UP_ok = false;
 	if(get_tile(g, 2, 0) != 2)
 		test_UP_ok = false;
 
 	// le score doit etre 28
-	if(grid_score(g) != 28)
+	if(grid_score(g) != 72)
 		test_UP_ok = false;
 
 	if(!test_UP_ok)
-		printf("erreur lors du do_move(UP) ou du grid_score(UP) ici le score doit valoir 28.(%d)\n", grid_score(g));
+		printf("erreur lors du do_move(UP) ou du grid_score(UP) ici le score doit valoir 72.(%lu)\n", grid_score(g));
 
 	/*
 	 *      la grille vaut : 
@@ -116,19 +116,19 @@ int main (int argc, char** argv)
 	 *      +--+--+--+--+ 
 	 */
 	do_move(g, RIGHT);
-	if(get_tile(g, 0, GRID_SIDE - 1) != 16)
+	if(get_tile(g, 0, GRID_SIDE - 1) != 5)
 		test_RIGHT_ok = false;
-	if(get_tile(g, 1, GRID_SIDE - 1) != 8)
+	if(get_tile(g, 1, GRID_SIDE - 1) != 4)
 		test_RIGHT_ok = false;
-	if(get_tile(g, 3, GRID_SIDE - 1) != 2)
+	if(get_tile(g, 2, GRID_SIDE - 1) != 2)
 		test_RIGHT_ok = false;
 
 	// le score doit etre 52
-	if(grid_score(g) != 52)
+	if(grid_score(g) != 104)
 		test_RIGHT_ok = false;
 
 	if(!test_RIGHT_ok)
-		printf("erreur lors du do_move(RIGHT) ou du grid_score(RIGHT) ici le score doit valoir 52.(%d)\n", grid_score(g));
+		printf("erreur lors du do_move(RIGHT) ou du grid_score(RIGHT) ici le score doit valoir 104.(%lu)\n", grid_score(g));
 	
 	/*
 	 *      la grille vaut : 
@@ -144,7 +144,7 @@ int main (int argc, char** argv)
 	 */
 	
 	// on triche un peu =)
-	set_tile(g, 3, GRID_SIDE - 1, 8);
+	set_tile(g, 2, GRID_SIDE - 1, 4);
 	/*
 	 *      la grille vaut : 
 	 *		+--+--+--+--+
@@ -157,18 +157,17 @@ int main (int argc, char** argv)
 	 *		| 0| 0| 0| 0| 
 	 *      +--+--+--+--+ 
 	 */
-
 	do_move(g, DOWN);
-	if(get_tile(g, GRID_SIDE - 2, GRID_SIDE - 1) != 16)
-		test_DOWN_ok = false;
-	if(get_tile(g, GRID_SIDE - 1, GRID_SIDE - 1) != 16)
+	if(get_tile(g, GRID_SIDE - 2, GRID_SIDE - 1) != 5)
+			test_DOWN_ok = false;
+	if(get_tile(g, GRID_SIDE - 1, GRID_SIDE - 1) != 5)
 		test_DOWN_ok = false;
 	// le score doit etre 68
-	if(grid_score(g) != 68)
+	if(grid_score(g) != 136)
 		test_DOWN_ok = false;
 
 	if(!test_DOWN_ok)
-		printf("erreur lors du do_move(DOWN) ou du grid_score(DOWN) ici le score doit valoir 68.(%d)\n", grid_score(g));
+		printf("erreur lors du do_move(DOWN) ou du grid_score(DOWN) ici le score doit valoir 136.(%lu)\n", grid_score(g));
 	/*
 	 *      la grille vaut : 
 	 *		+--+--+--+--+
@@ -183,14 +182,14 @@ int main (int argc, char** argv)
 	 */
 
 	do_move(g, DOWN);
-	if(get_tile(g, GRID_SIDE - 1, GRID_SIDE - 1) != 32)
+	if(get_tile(g, GRID_SIDE - 1, GRID_SIDE - 1) != 6)
 		test_DOWN_ok = false;
 	// le score doit etre 
-	if(grid_score(g) != 100)
+	if(grid_score(g) != 200)
 		test_DOWN_ok = false;
 
 	if(!test_DOWN_ok)
-		printf("erreur lors du do_move(DOWN) ou du grid_score(DOWN) ici le score doit valoir 100.(%d)\n", grid_score(g));
+		printf("erreur lors du do_move(DOWN) ou du grid_score(DOWN) ici le score doit valoir 200.(%lu)\n", grid_score(g));
 
 	/*
 	 *      la grille vaut : 
