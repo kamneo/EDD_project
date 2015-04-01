e2eaecc3b2296a3dc19c7ac242f72785599204e9
Raitei3 <antoine24.borde@gmail.com>
2015-04-01 12:46:32 +0200

    implementation strat trivial

--- src/stategy/strategy.c
+++ src/stategy/strategy.c
@@ -5,3 +5,30 @@ void free_memless_strat (strategy strat)
 {
   free (strat);
 }
+
+
+
+
+dir nextDirection(grid g){
+
+	if (can_move(g,LEFT)){
+		return LEFT;
+	}
+	else if (can_move(g,DOWN)){
+		return DOWN;
+	}
+	else if (can_move(g,UP)){
+		return UP;
+	}
+	else {
+		return RIGHT;
+	}
+}
+
+int main(void){
+	strategy strat = malloc(sizeof(strategy));
+	strat-> name = "Coins";
+	strat-> play_move = nextDirection;
+	strat->mem = null;
+	strat->void free_strategy=free_memless_strat;
+}
