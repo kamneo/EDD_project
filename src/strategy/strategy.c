d04af43feb7ac304d41a88ca9ba3026a43fd9835
kamneo <alexandre.pinero@gmail.com>
2015-04-01 09:15:25 -0400

    justement des CMakeLists

--- src/strategy/strategy.c
+++ src/strategy/strategy.c
@@ -2,72 +2,54 @@
 #include "stdlib.h"
 #include <assert.h>
 
-void free_memless_strat (strategy strat)
-{
-  free (strat);
+void free_memless_strat(strategy strat) {
+	free(strat);
 }
 
-
-
-
-dir nextDirectionTrivial1(strategy s,grid g){//variante de la strategy du point
-	int* nbTour=s->mem; //nombre de tour jouer
-	if ((*nbTour)%2==0){ 
-
-		if (can_move(g,LEFT)){
+dir nextDirectionTrivial1(strategy s, grid g) {
+	int* val = s->mem;
+	if ((*val) % 2 == 0) {
+		if (can_move(g, LEFT)) {
 			return LEFT;
-		}
-		else if (can_move(g,DOWN)){
+		} else if (can_move(g, DOWN)) {
 			return DOWN;
-		}
-		else if (can_move(g,RIGHT)){
+		} else if (can_move(g, RIGHT)) {
 			return RIGHT;
+		} else {
+			return UP;
 		}
-		else {
+	} else {
+		if (can_move(g, DOWN)) {
+			return DOWN;
+		} else if (can_move(g, LEFT)) {
+			return LEFT;
+		} else if (can_move(g, RIGHT)) {
+			return RIGHT;
+		} else {
 			return UP;
 		}
 	}
 
-
-	else{
-		if (can_move(g,DOWN)){
-			return DOWN;
-		}
-		else if (can_move(g,LEFT)){
-			return LEFT;
-		}
-		else if (can_move(g,RIGHT)){
-			return RIGHT;
-		}
-		else {
-			return UP;
-		}
-	}
-
-	(*nbTour)++;
+	(*val)++;
 }
 
+dir nextDirectionTrivial2(strategy s, grid g) {
 
-dir nextDirectionTrivial2(strategy s,grid g){ // strategy simple du coin
-
-	if (can_move(g,LEFT)){
+	if (can_move(g, LEFT)) {
 		return LEFT;
-	}
-	else if (can_move(g,DOWN)){
+	} else if (can_move(g, DOWN)) {
 		return DOWN;
-	}
-	else if (can_move(g,RIGHT)){
+	} else if (can_move(g, RIGHT)) {
 		return RIGHT;
-	}
-	else {
+	} else {
 		return UP;
 	}
 }
 
 /*int main(void){
-	strategy strat = malloc(sizeof(strategy));
-	strat-> name = "Coins";
-	strat-> play_move = nextDirection;
-	strat->mem = NULL;
-	strat->void free_strategy=free_memless_strat;
-}*/
+ strategy strat = malloc(sizeof(strategy));
+ strat-> name = "Coins";
+ strat-> play_move = nextDirection;
+ strat->mem = NULL;
+ strat->void free_strategy=free_memless_strat;
+ }*/
