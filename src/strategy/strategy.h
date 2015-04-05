42c5f529334133c91df619cab90fbf3f5267b0af
kamneo <alexandre.pinero@gmail.com>
2015-04-05 06:30:14 -0400

    Modification des premières stratégies et ajout d'une esquisse de eval() ajout de fonctionnalité de statistiques dans 2048_ncurses_IA.c

--- src/strategy/strategy.h
+++ src/strategy/strategy.h
@@ -9,11 +9,8 @@
 
 #include "grid.h"
 
-
-
 /** \b strategy is a pointer to a strategy_s structure */
 typedef struct strategy_s *strategy;
-
 
 /**
  * \b Structure that contains a strategy.
@@ -23,20 +20,33 @@ typedef struct strategy_s *strategy;
  * may require a history of previously played moves (or even games) ; in such
  * cases, a strategy may use the <mem> field to store such information.
  */
-struct strategy_s
-{
-  void* mem;  /** Points the the data stored by the strategy. */
-  char* name; /** The strategy's name. This will be used to display the tournament scores. */
-  dir (*play_move) (strategy, grid); /** A function which returns the direction chosen by the strategy. The first parameter is a pointer to the current strategy (useless for memoryless strategies). The second one is the current grid. */
-  void (*free_strategy) (strategy); /** Frees any resources allocated during the strategy's creation (<name>, <mem>, ...). */
+struct strategy_s {
+	void* mem; /** Points the the data stored by the strategy. */
+	char* name; /** The strategy's name. This will be used to display the tournament scores. */
+	dir (*play_move)(strategy, grid); /** A function which returns the direction chosen by the strategy. The first parameter is a pointer to the current strategy (useless for memoryless strategies). The second one is the current grid. */
+	void (*free_strategy)(strategy); /** Frees any resources allocated during the strategy's creation (<name>, <mem>, ...). */
 };
 
-dir nextDirectionTrivial1(strategy s,grid g);
-dir nextDirectionTrivial2(strategy s,grid g);
+/*
+ * deuwième strategie qui reprend la stratégie du coin mais qui un tour sur deux
+ * changera l'ordre des mouvements favori.
+ * param : strategy s la sutructure stratégie
+ * param : grid la grille
+ * return: la direction optimal à jouer qui a été calculé par cette stratégie
+ */
+dir strategie_coin_2(strategy s, grid g);
+
+/*
+ * premiere strategie appeler stratégie du coin
+ * param : strategy s la sutructure stratégie
+ * param : grid la grille
+ * return: la direction optimal à jouer qui a été calculé par cette stratégie
+ */
+dir strategie_coin_1(strategy s, grid g);
 
 /**
  * Naively frees the <strat> pointer.
  */
-extern void free_memless_strat (strategy strat);
+extern void free_memless_strat(strategy strat);
 
 #endif /* STRATEGY_H_ */
